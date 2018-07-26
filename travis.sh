e9fc1fcdfd61419a638c0c0aa42d1ddaf5c877ad
Jeroen Peeters <contact@jeroenpeeters.me>
2018-07-26 10:00:02 +0200

    CI Pipeline (#6)
    * chore: add basic testing
    
    * feat: add .travis.yml
    
    * chore: fix deploy to master
    
    * chore: commit version

---
+++ travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script that Travis CI will run to test the project, it is conditional on the branch.
+
+VERSION=$(<./VERSION)
+REPO=neobelgium/neo-local-faucet
+
+set -e
+
+if [[ $TRAVIS_BRANCH == 'master' ]]
+then
+  docker login -u $DOCKER_USER -p $DOCKER_PASS
+  docker build -f Dockerfile -t $REPO:latest .
+  docker tag $REPO:latest $REPO:$VERSION
+  docker push $REPO
+  git checkout master
+  git pull origin master
+  git commit -a -m $VERSION
+  git tag $VERSION
+  git push origin $BRANCH --tags
+fi
